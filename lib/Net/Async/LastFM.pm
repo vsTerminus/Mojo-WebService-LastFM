4453c3d051f796b9682030123e09edfc678bf0d1
vsTerminus <vsTerminus@vsTerminus.ca>
2017-01-27 10:28:58 -0600

    A few changes to the nowplaying sub
    - Now takes a hashref of parameters instead of requiring things to be in a specific order.
    - Returning a formatted string is now optional, based on whether the called defines the 'format' parameter
    - If format is not specified, the function will return a hashref containing artist, album, and title.

--- lib/Net/Async/LastFM.pm
+++ lib/Net/Async/LastFM.pm
@@ -29,9 +29,16 @@ sub new
 # The subroutine will call itself, decrementing the current retries value until it reaches zero and then send "Nothing playing for $user" to the callback.
 # If it succeeds, it will send the nowplaying info back to the callback function.
 
+# If the format parameter is passed in the sub will return a string in the specified format
+# If not, the sub will return a hashref with artist, album, and title.
 sub nowplaying
 {
-    my ($self, $user, $format, $callback, $retries) = @_;
+    my ($self, $params) = @_; #$user, $format, $callback, $retries) = @_;
+
+    my $user = $params->{'user'};
+    my $format = $params->{'format'};
+    my $callback = $params->{'callback'};
+    my $retries = $params->{'retries'};
 
     my $base_url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks';
     my $api_key = $self->{'api_key'};
@@ -49,16 +56,27 @@ sub nowplaying
         my $artist = $track->{'artist'}{'#text'};
         my $title = $track->{'name'};
         my $album = $track->{'album'}{'#text'};
-    
-        $format = "%artist% - %title%" unless defined $format;
-
+        
         if ( defined $artist and defined $album and defined $title )
         {
-            $format =~ s/%artist%/$artist/g;
-            $format =~ s/%album%/$album/g;
-            $format =~ s/%title%/$title/g;
+            # If the caller wants the results returns in a string format, do that now.
+            if ( defined $format )
+            {
+                $format =~ s/%artist%/$artist/g;
+                $format =~ s/%album%/$album/g;
+                $format =~ s/%title%/$title/g;
     
-            $np = $format;
+                $np = $format;
+            }
+            # If we were not given a format, just return a hashref with artist, album, and title.
+            else
+            {
+                $np = {
+                    'artist' => $artist,
+                    'album'  => $album,
+                    'title'  => $title,
+                };
+            }
         }
 
         if ( defined $np )
@@ -68,13 +86,33 @@ sub nowplaying
         elsif ( --$retries > 0 )
         {
             # Try again.
-            $self->nowplaying($user, $format, $callback, $retries);
+            $self->nowplaying($user, { format => $format, callback => $callback, retries => $retries});
         }
         else
         {
             $callback->("Nothing playing for $user");
         }
     });
+}
+
+# Get user info
+sub getinfo
+{
+    my ($self, $user, $callback) = @_;
+
+    my $base_url = "https://ws.audioscrobbler.com/2.0/?method=user.getinfo";
+
+    my $api_key = $self->{'api_key'};
+    my $api_url = $base_url . "&user=$user&api_key=$api_key&format=json";
+    
+    $self->{'ua'}->get($api_url => sub
+    {
+        my ($ua, $tx) = @_;
+
+        my  $json = $tx->res->json;
+        $callback->($json);
+    });
+
 }
 
 1;
